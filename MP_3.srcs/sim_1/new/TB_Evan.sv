618d662418131ed6eb6f6659875cbee50e8624ac
Evan McPheron <evancmcpheron@gmail.com>
2025-09-15 23:44:21 -0500

    finished Bit adder and sim

--- MP_3.srcs/sim_1/new/TB_Evan.sv
+++ MP_3.srcs/sim_1/new/TB_Evan.sv
@@ -23,6 +23,7 @@ module TB_Evan();
 timeunit 10ns;
 timeprecision 1ns;
 
+// DUT ripple_adder
 logic [15:0] a; // Input Logic
 logic [15:0] b;
 
@@ -30,15 +31,50 @@ logic [15:0] s; //output logic
 logic cin;
 logic cout;
 
-rip
-
 assign cin = 0; // we can assume assign is zero
 
+ripple_adder adder (
+  .a (a),
+  .b (b),
+  .cin (cin),
+  .s (s),
+  .cout (cout)
+);
+
+// DUT full bit adder (shares cin/cout logics)
+logic bit_a;
+logic bit_b;
+logic bit_s;
+full_bit_adder bit_adder (
+  .a (bit_a),
+  .b (bit_b),
+  .s (bit_s),
+  .cin (cin),
+  .cout (cout)
+);
+
 initial begin: TESTS
-  a = 16'h2222
-  b = 16'h3333
+  s = 16'h0000;
+  a = 16'h2222;
+  b = 16'h3333;
 
+  #5 $display("Output value is %h with a c-value of $b", s, b);
+
+  bit_a = 1'b1;
+  bit_b = 1'b1;
   
+  #2 assert (bit_s == 1'b1 && cout == 1'b1) else $display("a=1, b=1 incorrect!");
 
+  bit_a = 1'b0;
+  bit_b = 1'b1;
+  
+  #2 assert (bit_s == 1'b1 && cout == 1'b0) else $display("a=1, b=0 incorrect!");
+
+  bit_a = 1'b0;
+  bit_b = 1'b0;
+  
+  #2 assert (bit_s == 1'b0 && cout == 1'b0) else $display("a=0, b=0 incorrect!");
+
+  $finish();
 end
 endmodule
